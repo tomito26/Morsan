5228ea78a5d2256717c49775004ab29fb8c8dd04
tomito26 <tommybwah@gmail.com>
2025-01-20 10:36:09 +0300

    style: redesigned the why choose us section

--- components/home-page/why-choose-us.tsx
+++ components/home-page/why-choose-us.tsx
@@ -1,39 +1,58 @@
-import { Check } from "lucide-react";
-import React from "react";
-import Image from "next/image";
 import { our_benefits } from "@/lib/constants";
+import Image from "next/image";
 
 const WhyChooseUs = () => {
   return (
-    <div className="bg-[#0B2E4A] flex-col-reverse md:flex-row-reverse flex w-full gap-10">
-      <div className="h-[300px] md:h-auto w-full md:w-1/2 relative shrink-0">
-        <Image
-          src="/assets/successful-business-team-posing-office-hall.jpg"
-          fill
-          alt="Why choose us"
-          className="absolute object-cover shrink-0"
-        />
+    <div className="bg-[#0B2E4A] flex-col items-center md:flex-row-reverse flex w-full gap-10 md:px-[40px] lg:px-[60px] xl:px-[100px] 2xl:px-[160px] 3xl:px-[200px] 4xl:px-[240px] 5xl:px-[320px] px-4 py-24">
+      <div className="flex-1">
+        <h1 className="text-white uppercase font-bold text-sm">
+          Why Choose Us
+        </h1>
+        <h2 className="text-2xl md:text-5xl font-bold text-white mt-4 mb-5">
+          Exceptional HR Solutions, Tailored for Your Success
+        </h2>
+        <p className="text-base text-white mb-6">
+          We are dedicated to delivering personalized human resource solutions
+          that empower businesses to excel. With a focus on efficiency,
+          innovation, and measurable results, we ensure your organization
+          attracts, retains, and develops top talent while optimizing HR
+          operations to meet your unique needs.
+        </p>
+        <div className="h-[320px] w-full relative shrink-0 rounded-md">
+          <Image
+            src="/assets/Black-Team-687x458-1.jpg"
+            fill
+            alt="Why choose us"
+            className="absolute object-cover shrink-0 rounded-md"
+          />
+        </div>
       </div>
-      <div className="flex-1 py-16 xl:pl-[100px] 2xl:pl-[160px] 3xl:pl-[200px] 4xl:pl[240px] 5xl:pl-[320px] px-4">
+
+      <div className="flex-1 ">
         <div className="flex-1">
-          <h1 className="text-white uppercase font-bold text-sm">
-            Why Choose Us
-          </h1>
-          <h2 className="text-2xl md:text-5xl font-bold text-white mt-4 mb-5">
-            Exceptional HR Solutions, Tailored for Your Success
-          </h2>
-          <div>
+          <div className="divide-y">
             {our_benefits.map((benefit) => (
-              <div key={benefit.title} className="mb-3">
-                <div className="flex items-center gap-3">
-                  <Check className="h-5 w-5 stroke-white" />
-                  <h3 className="text-lg text-white font-bold">
-                    {benefit.title}
-                  </h3>
+              <div key={benefit.title} className="py-4">
+                <div className="flex md:flex-row flex-col items-center gap-4">
+                  <div className="flex justify-center">
+                    <div className="h-12 w-12 relative shrink-0">
+                      <Image
+                        src={benefit.icon}
+                        alt={benefit.title}
+                        fill
+                        className="absolute object-cover"
+                      />
+                    </div>
+                  </div>
+                  <div>
+                    <h3 className="text-lg text-white md:text-left text-center  font-bold">
+                      {benefit.title}
+                    </h3>
+                    <p className="text-white md:text-left text-center  text-base ">
+                      {benefit.description}
+                    </p>
+                  </div>
                 </div>
-                <p className="text-white text-base ml-8">
-                  {benefit.description}
-                </p>
               </div>
             ))}
           </div>
--- lib/constants.ts
+++ lib/constants.ts
@@ -136,32 +136,37 @@ export const our_benefits = [
     title: "Proven Expertise",
     description:
       "Our team comprises seasoned HR professionals with extensive experience across diverse industries",
+    icon: "/assets/result.png"
   },
   {
     title: "Customized Approach",
     description:
       " Our services are personalized to align with your goals, culture, and challenges.",
+    icon: "/assets/hand.png"
   },
   {
     title: "Comprehensive Solutions",
     description:
       "From talent acquisition and HR support to compliance management and employee engagement, we provide end-to-end HR solutions",
+    icon: "/assets/puzzle.png"
   },
   {
     title: "Time and Cost Efficiency",
     description:
       "Outsource your HR needs to us and save time, reduce overhead costs, and gain access to premium services without the hassle of building an in-house HR team.",
+    icon: "/assets/money-strategy.png"
   },
   {
     title: "Focus on Results",
     description:
       "Our strategies are designed to deliver measurable outcomes—whether it's attracting top talent, reducing turnover, or improving employee productivity.",
+    icon: "/assets/evaluation.png"
   },
-  {
-    title: "Technology Integration",
-    description:
-      "We leverage modern HR tools and systems to streamline processes, enhance efficiency, and provide real-time insights into your workforce.",
-  },
+  // {
+  //   title: "Technology Integration",
+  //   description:
+  //     "We leverage modern HR tools and systems to streamline processes, enhance efficiency, and provide real-time insights into your workforce.",
+  // },
 ];
 
 export const industries = [
